06081078e62740962459c745beedce0017d1c62a
ericknathan <erick.capito@hotmail.com>
2021-11-25 09:58:37 -0300

    feat: header finalizado

--- components/header/header.php
+++ components/header/header.php
@@ -7,19 +7,22 @@
         <i class="fas fa-bars"></i>
     </a>
     <img src="<?= $root ?>public/images/logo.svg" alt="Soft Cake">
-    <ul id="navbar">
-        <li>Catálogo</li>
-        <li>Sobre nós</li>
-        <li>
-            <img src="<?= $root ?>public/images/logo.svg" alt="Soft Cake">
-        </li>
-        <li>Unidades</li>
-        <li>
-            <?php if(isset($_SESSION['']))  {?>
-                <!-- <button>Login</button> -->
-            <?php } else { ?>
-                <button>Login</button>
-            <?php } ?>
-        </li>
-    </ul>
+    <div id="navbar">
+        <ul>
+            <li>Catálogo</li>
+            <li>Sobre nós</li>
+            <li class="logo">
+                <img src="<?= $root ?>public/images/logo.svg" alt="Soft Cake">
+            </li>
+            <li>Unidades</li>
+            <li>
+                <?php if(isset($_SESSION['']))  {?>
+                    <!-- <button>Login</button> -->
+                <?php } else { ?>
+                    <button>Login</button>
+                <?php } ?>
+            </li>
+        </ul>
+        <a class="background" href="#"></a>
+    </div>
 </header>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    $root = $_SERVER['REQUEST_URI'];
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -5,8 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Soft Cake</title>
     <script src="" defer></script>
-    <link rel="stylesheet" href="./public/styles/global.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="<?= $root ?>public/styles/global.css">
+    <link rel="stylesheet" href="<?= $root ?>style.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
     <script src="https://kit.fontawesome.com/7cbb7ae6e7.js" crossorigin="anonymous"></script>
