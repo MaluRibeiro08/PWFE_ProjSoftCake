993cd0bc745de703e1cde5c70e6772eaf4911c5f
ericknathan <erick.capito@hotmail.com>
2021-12-07 20:20:11 -0300

    fix: listagem de bolos após correção da api

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const items = document.querySelectorAll(".item-pergunta button");
 
+var produtos;
+
 function toggleFAQ() {
   const itemToggle = this.getAttribute('aria-expanded');
   
@@ -10,36 +12,42 @@ function toggleFAQ() {
 
 items.forEach(item => item.addEventListener('click', toggleFAQ));
 
-fetch('http://localhost/softcake/backend/v1/bolo/', {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-}).then(response => response.json()).then(produtos => { 
-  const divProdutos = document.querySelector('.produtos');
+function renderCakes() {
+  fetch('http://localhost/softcake/backend/v1/bolo/', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(response => response.json()).then(listaProdutos => { 
+    const divProdutos = document.querySelector('.produtos');
+  
+    produtos = Object.values(listaProdutos);
 
-  produtos.forEach(produto => {
-    const div = document.createElement('div');
-    div.classList.add('card-produto');
-    div.innerHTML = `
-      <img src="https://images.unsplash.com/photo-1614174486496-344ef3e9d870?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80" alt="Bolo de chocolate">
-      <div class="informacoes">
-          <div class="titulo">
-              <h4>${produto.nomeCard}</h4>
-              <h5>R$${produto.precoPorQuilo}</h5>
-          </div>
-          <div class="estrelas">
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-          </div>
-          <button onclick="window.location.href='/produto?id=${produto.idBolo}'">
-              <i class="fas fa-shopping-cart"></i>
-          </button>
-      </div>
-    `;
-    divProdutos.appendChild(div);
+    produtos.forEach(produto => {
+      const div = document.createElement('div');
+      div.classList.add('card-produto');
+      div.innerHTML = `
+        <img src="${produto.imagens[0]}" alt="Imagem ilustrativa de ${produto.nomeCard}">
+        <div class="informacoes">
+            <div class="titulo">
+                <h4>${produto.nomeCard}</h4>
+                <h5>R$${produto.precoPorQuilo}</h5>
+            </div>
+            <div class="estrelas">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <button type="button" onclick="window.location.href='/produto/?id=${produto.idBolo}'">
+                <i class="fas fa-shopping-cart"></i>
+            </button>
+        </div>
+      `;
+      divProdutos.appendChild(div);
+    });
   });
-});
+}
+
+renderCakes();
