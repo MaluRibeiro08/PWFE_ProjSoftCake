f7a2037069df3d3c1bec1a5d336ee188c6d16a58
ericknathan <erick.capito@hotmail.com>
2021-12-10 11:32:20 -0300

    feat: Categorias pré-implementadas na criação do produto

--- produto/cadastro/scripts/script.js
+++ produto/cadastro/scripts/script.js
@@ -42,3 +42,39 @@ const verificaCamposUploadDisponiveis = (numeroUltimoInputUsado) =>
 
 document.getElementById("inputFile1").addEventListener("change", tratarImagem)
 document.getElementById("inputFile2").addEventListener("change", tratarImagem)
+
+const adicionarCategoria = () => {};
+
+const carregarCategorias = () => {
+    fetch('http://localhost/softcake/backend/v1/ingrediente/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => response.json()).then(listaIngredientes => {
+        const containerIngredientes = document.querySelector('.categorias');
+
+        const ingredientes = Object.values(listaIngredientes.message);
+        ingredientes.forEach(ingrediente => {
+            const div = document.createElement('div');
+            div.classList.add('categoria');
+            div.innerHTML = `
+                <input type="checkbox" name="${ingrediente.idIngrediente}" id="${ingrediente.idIngrediente}">
+                <label for="${ingrediente.idIngrediente}">${ingrediente.nome}</label>
+            `;
+            containerIngredientes.appendChild(div);
+        });
+
+        const div = document.createElement('div');
+        div.classList.add('categoria');
+        div.innerHTML = `
+            <input type="text" placeholder="Nova categoria" name="nova-categoria" id="nova-categoria">
+            <button type="button" id="adicionar-categoria">Adicionar</button>
+        `;
+        div.addEventListener('click', adicionarCategoria);
+
+        containerIngredientes.appendChild(div);
+    });
+}
+
+carregarCategorias()
